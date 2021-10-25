5a1fca23777eb9c63823f6dc1725244bc2d4335e
Jiri Petrlik <jiripetrlik@gmail.com>
2021-10-25 22:24:17 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="evokit",
+    version="0.1",
+    packages=find_packages(),
+    author="Jiri Petrlik",
+    author_email="jiripetrlik@gmail.com",
+    install_requires=[],
+)
