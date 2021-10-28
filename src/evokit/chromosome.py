d37de8ab395b950bf9b0dbaee19d31ddf59818f2
Jiri Petrlik <jiripetrlik@gmail.com>
2021-10-28 16:12:03 +0200

    Make test deterministic

--- src/evokit/chromosome.py
+++ src/evokit/chromosome.py
@@ -6,15 +6,10 @@ class Chromosome(ABC):
         pass
 
 class NumericChromosome(Chromosome):
-    def __init__(self, size, minValues, maxValues, randomGenerator = None):
+    def __init__(self, size, minValues, maxValues):
         self.values = np.zeros(size)
         self.minValues = minValues
         self.maxValues = maxValues
 
-        if randomGenerator != None:
-            self.randomGenerator = randomGenerator
-        else:
-            self.randomGenerator = np.random.default_rng()
-
     def initialize(self):
-        self.values = self.randomGenerator.uniform(self.minValues, self.maxValues, len(self.values))
+        self.values = np.random.uniform(self.minValues, self.maxValues, len(self.values))
--- tests/test_chromosome.py
+++ tests/test_chromosome.py
@@ -1,6 +1,9 @@
+import numpy as np
 import evokit.chromosome as ch
 
 def test_initialize_numeric_chromosome():
+    np.random.seed(0)
+
     chromosome = ch.NumericChromosome(5, 1, 7)
     chromosome.initialize()
     for _ in range(100):
