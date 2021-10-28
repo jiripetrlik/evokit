f8159330636c1b7d891bb11317d65a8d29e6e0dd
Jiri Petrlik <jiripetrlik@gmail.com>
2021-10-28 20:50:02 +0200

    Extend test coverage for NumericChromosome

--- tests/test_chromosome.py
+++ tests/test_chromosome.py
@@ -1,7 +1,7 @@
 import numpy as np
 import evokit.chromosome as ch
 
-def test_initialize_numeric_chromosome():
+def test_initialize_numeric_chromosome_1():
     np.random.seed(0)
 
     chromosome = ch.NumericChromosome(5, 1, 7)
@@ -10,3 +10,27 @@ def test_initialize_numeric_chromosome():
         for i in range(len(chromosome.values)):
             assert chromosome.values[i] >= 1
             assert chromosome.values[i] <= 7
+
+def test_initialize_numeric_chromosome_2():
+    np.random.seed(0)
+
+    min = np.array([-1, 2.5, 4])
+    max = np.array([-0.5, 9, 150])
+    chromosome = ch.NumericChromosome(3, min, max)
+    chromosome.initialize()
+    for _ in range(100):
+        for i in range(len(chromosome.values)):
+            assert chromosome.values[i] >= min[i]
+            assert chromosome.values[i] <= max[i]
+
+def test_initialize_numeric_chromosome_3():
+    np.random.seed(0)
+
+    min = -5
+    max = np.array([-0.5, 9, 150])
+    chromosome = ch.NumericChromosome(3, min, max)
+    chromosome.initialize()
+    for _ in range(100):
+        for i in range(len(chromosome.values)):
+            assert chromosome.values[i] >= min
+            assert chromosome.values[i] <= max[i]
