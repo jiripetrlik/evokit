b38ea07b81a775d541240668c694f4591708767a
Jiri Petrlik <jiripetrlik@gmail.com>
2021-10-28 15:22:04 +0200

    Improve test for numeric chromosome

--- tests/test_chromosome.py
+++ tests/test_chromosome.py
@@ -1,8 +1,9 @@
-import evokit.chromosome
+import evokit.chromosome as ch
 
 def test_initialize_numeric_chromosome():
-    chromosome = evokit.chromosome.NumericChromosome(5, 1, 7)
+    chromosome = ch.NumericChromosome(5, 1, 7)
     chromosome.initialize()
-    for i in range(len(chromosome.values)):
-        assert chromosome.values[i] >= 1
-        assert chromosome.values[i] <= 7
+    for _ in range(100):
+        for i in range(len(chromosome.values)):
+            assert chromosome.values[i] >= 1
+            assert chromosome.values[i] <= 7
