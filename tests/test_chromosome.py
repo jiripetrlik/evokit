44dde54e7b1618d046b8ead0c8eea628ccd1727d
Jiri Petrlik <jiripetrlik@gmail.com>
2021-10-31 13:40:25 +0100

    Extend test coverage for NumericChromosome

--- tests/test_chromosome.py
+++ tests/test_chromosome.py
@@ -34,3 +34,25 @@ def test_initialize_numeric_chromosome_3():
         for i in range(len(chromosome.values)):
             assert chromosome.values[i] >= min
             assert chromosome.values[i] <= max[i]
+
+def test_renormalize():
+    min = -5
+    max = np.array([-0.5, 9, 150])
+    chromosome = ch.NumericChromosome(3, min, max)
+    chromosome.values = np.array([-7, 10, 151])
+    chromosome.renormalize()
+    assert np.array_equal(chromosome.values, [-5, 9, 150])
+
+def test_copyValues():
+    np.random.seed(0)
+
+    chromosome1 = ch.NumericChromosome(5, -1, 2)
+    chromosome1.initialize()
+    chromosome2 = ch.NumericChromosome(5, -1, 2)
+    chromosome2.initialize()
+    assert chromosome1.values is not chromosome2.values
+    assert np.array_equal(chromosome1.values, chromosome2.values) == False
+    
+    chromosome1.copyValues(chromosome2)
+    assert chromosome1.values is not chromosome2.values
+    assert np.array_equal(chromosome1.values, chromosome2.values)
