b8aa414b616d6e255dbb703f297386d8bcbf0dca
Jiri Petrlik <jiripetrlik@gmail.com>
2021-11-07 11:18:14 +0100

    Introduce module for benchmarks

---
+++ src/evokit/benchmarks.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
+def sphereFunction(x):
+    x = np.array(x)
+    return np.sum(x ** 2)
+
+def plotBenchmark(benchmark, min = -10, max = 10):
+    x = np.arange(start = min, stop = max, step = 0.1, dtype=np.float64)
+    y = np.arange(start = min, stop = max, step = 0.1, dtype=np.float64)
+    x, y = np.meshgrid(x, y)
+    
+    z = np.zeros(np.shape(x), dtype=np.float64)
+    for i in range(np.shape(x)[0]):
+        for j in range(np.shape(x)[1]):
+            z[i, j] = benchmark(np.array([x[i, j], y[i, j]]))
+
+    fig = plt.figure()
+    ax = Axes3D(fig, auto_add_to_figure=False)
+    fig.add_axes(ax)
+    ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=cm.viridis)
+
+    plt.show()
