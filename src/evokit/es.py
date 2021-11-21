9e65ef68e927118cec647fe9d12923701a26aaab
Jiri Petrlik <jiripetrlik@gmail.com>
2021-11-21 10:59:05 +0100

    Introduce mi plus one ES

--- src/evokit/es.py
+++ src/evokit/es.py
@@ -39,3 +39,57 @@ def onePlusOneES(fitness, size, sd, iterations):
     }
 
     return results
+
+class miPlusOneObserver:
+    def __init__(self, iterations):
+        self.minFitness = np.zeros(iterations)
+        self.meanFitness = np.zeros(iterations)
+        self.maxFitness = np.zeros(iterations)
+        self.improved = np.zeros(iterations, dtype=np.bool_)
+
+    def update(self, iteration, parentsFitness, improved):
+        self.minFitness[iteration] = np.min(parentsFitness)
+        self.meanFitness[iteration] = np.mean(parentsFitness)
+        self.maxFitness[iteration] = np.max(parentsFitness)
+        self.improved[iteration] = improved
+
+def miPlusOneES(fitness, size, mi, iterations):
+    parents = np.random.normal(size=(mi, size))
+    parentsSD = np.random.normal(size=(mi, size))
+    parentsFitness = np.apply_along_axis(fitness, 1, parents)
+    worstSolution = np.argmax(parentsFitness)
+    worstFitness = parentsFitness[worstSolution]
+    observer = miPlusOneObserver(iterations)
+
+    for iter in range(iterations):
+        sample1 = np.random.randint(low = 0, high = mi, size = size)
+        sample2 = np.random.randint(low = 0, high = mi, size = size)
+        values1 = parents[sample1, range(size)]
+        values2 = parents[sample2, range(size)]
+        values = (values1 + values2) / 2
+        newFitness = fitness(values)
+        improved = False
+        if newFitness <= worstFitness:
+            sd1 = parentsSD[sample1, range(size)]
+            sd2 = parentsSD[sample2, range(size)]
+            sd = (sd1 + sd2) / 2
+            
+            parentsFitness[worstSolution] = newFitness
+            parents[worstSolution,:] = values
+            parentsSD[worstSolution,:] = sd
+            
+            improved = True
+            worstSolution = np.argmax(parentsFitness)
+            worstFitness = parentsFitness[worstSolution]
+
+        observer.update(iter, parentsFitness, improved)
+
+    bestSolution = np.argmin(parentsFitness)
+    bestFitness = parentsFitness[bestSolution]
+    results = {
+        "fitness": bestFitness,
+        "solution": parents[bestSolution],
+        "observer": observer
+    }
+
+    return results
