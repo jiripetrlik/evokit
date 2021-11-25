565376cade4465da048ea912a6dd1a283a490071
Jiri Petrlik <jiripetrlik@gmail.com>
2021-11-25 22:17:24 +0100

    Test for mi plus one ES

--- tests/test_es.py
+++ tests/test_es.py
@@ -12,3 +12,18 @@ def test_onePlusOneES():
         assert np.all(error < 0.1)
         assert len(result["observer"].minFitness) == iterations
         assert len(result["observer"].improved) == iterations
+
+def test_miPlusOneES():
+    np.random.seed(0)
+    fitness = lambda v : ((v[0] - 1) ** 2) + (v[1] ** 2) + ((v[2] + 1) ** 2)
+    iterations = 1000
+
+    for _ in range(5):
+        result = e.miPlusOneES(fitness, 3, 8, iterations)
+        assert result["fitness"] < 0.1
+        error = result["solution"] - np.array([1, 0, -1])
+        assert np.all(error < 0.1)
+        assert len(result["observer"].minFitness) == iterations
+        assert len(result["observer"].meanFitness) == iterations
+        assert len(result["observer"].maxFitness) == iterations
+        assert len(result["observer"].improved) == iterations
