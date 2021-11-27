06852f188dc6c96dd473f18a856498db4464a38c
Jiri Petrlik <jiripetrlik@gmail.com>
2021-11-27 15:55:21 +0100

    Create test for mi plus lambda ES

--- tests/test_es.py
+++ tests/test_es.py
@@ -28,3 +28,18 @@ def test_miPlusOneES():
         assert len(result["observer"].meanFitness) == iterations
         assert len(result["observer"].maxFitness) == iterations
         assert len(result["observer"].improved) == iterations
+
+def test_miPlusLambdaES():
+    np.random.seed(0)
+    fitness = lambda v : ((v[0] - 1) ** 2) + (v[1] ** 2) + ((v[2] + 1) ** 2)
+    iterations = 100
+
+    for _ in range(5):
+        result = e.miPlusLambdaES(fitness, 3, 8, 16, iterations)
+        assert result["fitness"] < 0.1
+        error = result["solution"] - np.array([1, 0, -1])
+        assert np.all(error < 0.1)
+        assert len(result["observer"].minFitness) == iterations
+        assert len(result["observer"].meanFitness) == iterations
+        assert len(result["observer"].maxFitness) == iterations
+        assert len(result["observer"].improved) == iterations
